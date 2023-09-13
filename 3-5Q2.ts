f1a1a9329a3eb5610d43b83a679bb09b314728e3
akamine <akamine.shinichiro@cloud-ace.jp>
2023-09-13 14:57:13 +0900

    3-5Q2の問題をコミット

---
+++ 3-5Q2.ts
@@ -0,0 +1,15 @@
+// 3-5 q2
+// 整数 A と B の最大公約数を出力するプログラムを作成してください。
+// ただし次の条件を満たすとき「 X は A と B の最大公約数である」と言います。
+// 条件：X は A も B も割り切る 1 以上の整数の中で最大のものである
+
+const a: number = 18;
+const b: number = 30;
+
+// 最大公約数を求める関数
+const gcd = (a, b) => {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
+
+console.log(gcd(a, b));
